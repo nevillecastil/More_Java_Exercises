3c8d170e8da8a76e96091c58e9771a21df411b23
Neville Castillo <58572945+nevillecastil@users.noreply.github.com>
2022-01-29 23:56:38 -0500

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,3 +1,11 @@
+/*
+Ken’s office hours are getting really full, and a long line is forming outside his office each time.
+The students decide that they are too smart to simply line up, and decide on implementing a system.  The system is a queue, but with some weird exceptions.  When a student joins the back of the line, if they are younger or if their grade is lower than the person in front of them they can move up 1 spot, but only 1 spot.  
+Input
+If a student is joining the line, the input format is the student’s first name then a space then their age, then a space, then their grade.  When Ken helps the next student in line, removing them from the queue, the input is next.  When the input is done, the input is end.
+Output
+The output will be the order of the students currently in line.   If the queue is empty, output the word empty.
+*/
 import java.util.ArrayList;
 import java.util.Scanner;
 
